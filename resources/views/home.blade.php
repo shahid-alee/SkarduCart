2df2405ff2ffcd6fa33a1dd132433c5091b9a59d
shahid-alee <shahidaliabidi10@gmail.com>
2026-02-20 21:52:40 +0500

    new project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,171 @@
+@extends('layouts.main')
+@section('content')
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="{{ asset('assets/images/slider3.png') }}" class="d-block w-100" alt="slider 1">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('assets/images/slider2.png') }}" class="d-block w-100" alt="slider 2">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('assets/images/slider1.png') }}" class="d-block w-100" alt="slider 3">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+
+<!-- product ssection -->
+
+<section class="my-5">
+    <div class="container">
+
+
+        <div class="d-flex bd-highlight">
+            <div class="flex-grow-1 bd-highlight"><h3>Top Deals</h3></div>
+            <div ><a href="#" class="btn btn-sm theme-green-btn  text-light">View All</a></div>
+            
+        </div>
+
+        <div class="row theme-product">
+            <div class="col-lg-3">
+                <div class="card">
+                    <a href="3"><img src="{{ asset('assets/images/products/1.jpg') }}" class="card-img-top" alt="product"></a>
+                    <div class="card-body">
+                        <a href="#" class="text-dark text-decoration-none">
+                            <h6 class="card-title text-center">Zero lifestyle watch</h6>
+                        </a>
+                        <h5 class="card-text text-center">Rs 4,999</h5>
+                        <!-- <a href="#" class="btn btn-primary">Buy Now</a> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <a href="#"><img src="{{ asset('assets/images/products/2.jpg') }}" class="card-img-top" alt="product"></a>
+                    <div class="card-body">
+                        <a href="#" class="text-dark text-decoration-none">
+                            <h6 class="card-title text-center">Head phone</h6>
+                        </a>
+                        <h5 class="card-text text-center">Rs 1,999</h5>
+                        <!-- <a href="#" class="btn btn-primary">Buy Now</a> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <a href="#" class="text-dark text-decoration-none"><img src="{{ asset('assets/images/products/3.jpg') }}" class="card-img-top" alt="product"></a>
+                    <div class="card-body">
+                        <a href="#" class="text-dark text-decoration-none">
+                            <h6 class="card-title text-center">Nike shoes</h6>
+                        </a>
+                        <h5 class="card-text text-center">Rs 2,999</h5>
+                        <!-- <a href="#" class="btn btn-primary">Buy Now</a> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <a href="#" class="text-dark text-decoration-none"><img src="{{ asset('assets/images/products/8.jpg') }}" class="card-img-top" alt="product"></a>
+                    <div class="card-body">
+                        <a href="#"></a>
+                        <h6 class="card-title text-center">Apple airbads</h6></a>
+                        <h5 class="card-text text-center">Rs 4,999</h5>
+                        <!-- <a href="#" class="btn btn-primary">Buy Now</a> -->
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+</section>
+
+<!-- recently veiwed -->
+
+<section class="my-5">
+    <div class="container">
+
+
+        <div class="d-flex bd-highlight">
+            <div class="flex-grow-1 bd-highlight"><h3>Recently viewed</h3></div>
+            <div ><a href="#" class="btn btn-sm theme-orange-btn  text-light">View All</a></div>
+            
+        </div>
+
+        <div class="row theme-product">
+            <div class="col-lg-3">
+                <div class="card">
+                    <a href="3"><img src="{{ asset('assets/images/products/32.jpg') }}" class="card-img-top" alt="product"></a>
+                    <div class="card-body">
+                        <a href="#" class="text-dark text-decoration-none">
+                            <h6 class="card-title text-center">Mini MicroWave Oven</h6>
+                        </a>
+                        <h5 class="card-text text-center">Rs 14,999</h5>
+                        <!-- <a href="#" class="btn btn-primary">Buy Now</a> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <a href="#"><img src="{{ asset('assets/images/products/26.jpg') }}" class="card-img-top" alt="product"></a>
+                    <div class="card-body">
+                        <a href="#" class="text-dark text-decoration-none">
+                            <h6 class="card-title text-center">living room set</h6>
+                        </a>
+                        <h5 class="card-text text-center">Rs 111,999</h5>
+                        <!-- <a href="#" class="btn btn-primary">Buy Now</a> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <a href="#" ><img src="{{ asset('assets/images/products/31.jpg') }}" class="card-img-top" alt="product"></a>
+                    <div class="card-body">
+                        <a href="#" class="text-dark text-decoration-none">
+                            <h6 class="card-title text-center">Digital stove</h6>
+                        </a>
+                        <h5 class="card-text text-center">Rs 2,999</h5>
+                        <!-- <a href="#" class="btn btn-primary">Buy Now</a> -->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3">
+                <div class="card">
+                    <a href="#" ><img src="{{ asset('assets/images/products/18.jpg') }}" class="card-img-top" alt="product"></a>
+                    <div class="card-body">
+                        <a href="#" class="text-dark text-decoration-none">
+                        <h6 class="card-title text-center">Fancy girl sweater</h6></a>
+                        <h5 class="card-text text-center">Rs 1,499</h5>
+                        <!-- <a href="#" class="btn btn-primary">Buy Now</a> -->
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+</section>
+
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+
+    <title>Home page</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg theme-navbar">
+        <div class="container">
+            <a class="navbar-brand text-light" href="#">
+                <img src="{{ asset('assets/images/logos/logo2.png') }}" class="card-img-top" alt="Logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div>
+                <form class="d-flex">
+                    <div class="input-group">
+                        <input class="form-control form-control-sm " style="width: 350px;" type="search" placeholder="Search Products" aria-label="Search">
+                        <button class="btn btn-light btn-sm text-secondary" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <a href="#" class="text-decoration-none text-light">Become a seller</a>
+                <a href="#" class="btn theme-green-btn btn-sm text-light"><i class="fa-solid fa-cart-arrow-down"></i>Cart</a>
+                <a href="#" class="btn theme-orange-btn btn-sm text-light"><i class="fa-solid fa-user"></i>Login</a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- category Nav Bar -->
+
+    <nav class="navbar navbar-expand-lg theme-navbar-light">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link active text-dark" href="#">Mobile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-dark" href="#">Fashion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-dark" href="#">Electronics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-dark" href="#">Furniture</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-dark" href="#">Grocery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-dark" href="#">Appliances</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+    </nav>
